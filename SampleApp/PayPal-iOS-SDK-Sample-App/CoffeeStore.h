ba0ce471b290d2780e4cd46250e9fcf1bbcac93f
tony <tony@tonys-MacBook-Pro.local>
2018-10-10 17:33:20 +0800

    first commit

---
+++ SampleApp/PayPal-iOS-SDK-Sample-App/CoffeeStore.h
@@ -0,0 +1,13 @@
+//
+//  CoffeeStore.h
+//  PayPal-iOS-SDK-Sample-App
+//
+//  Created by tony on 10/10/2018.
+//  Copyright © 2018 PayPal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoffeeStore : UIViewController
+
+@end
---
+++ SampleApp/PayPal-iOS-SDK-Sample-App/CoffeeStore.m
@@ -0,0 +1,37 @@
+//
+//  CoffeeStore.m
+//  PayPal-iOS-SDK-Sample-App
+//
+//  Created by tony on 10/10/2018.
+//  Copyright © 2018 PayPal. All rights reserved.
+//
+
+#import "CoffeeStore.h"
+
+@interface CoffeeStore ()
+
+@end
+
+@implementation CoffeeStore
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
